7561fffc9949fca32cf7cd803b992e26490e5d7c
Jordan Mor <jordanmor@gmail.com>
2019-07-24 15:07:13 -0400

    Add static project design files and templates

--- src/main/java/com/treehouse/giflib/controller/GifController.java
+++ src/main/java/com/treehouse/giflib/controller/GifController.java
@@ -2,20 +2,12 @@ package com.treehouse.giflib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "List of all the GIFs!";
-    }
-
-    @RequestMapping("/gif")
-    @ResponseBody
-    public String showGifDetails() {
-        return "Show GIF details";
+        return "home";
     }
 }
