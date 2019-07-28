904fc3fd8a01a0850efde5bf1e9ac016adcf71f8
Jordan Mor <jordanmor@gmail.com>
2019-07-27 20:17:20 -0400

    Add reference of relevant category id to each GIF

--- src/main/java/com/tts/giflib/data/CategoryRepository.java
+++ src/main/java/com/tts/giflib/data/CategoryRepository.java
@@ -9,12 +9,21 @@ import java.util.List;
 @Component
 public class CategoryRepository {
     private static final List<Category> ALL_CATEGORIES = Arrays.asList(
-            new Category(1, "silly"),
-            new Category(2, "colorful"),
-            new Category(3, "animated")
+            new Category(1, "Robots"),
+            new Category(2, "People"),
+            new Category(3, "Animated")
     );
 
     public List<Category> findAll() {
         return ALL_CATEGORIES;
+    }
+
+    public Category findById(int id) {
+        for(Category category : ALL_CATEGORIES) {
+            if(category.getId() == id) {
+                return category;
+            }
+        }
+        return null;
     }
 }
